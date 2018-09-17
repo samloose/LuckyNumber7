da4937a3b8b0d9a912aed7aebcc79314dd9e239f
sam.loose <sloose@jaguar.csuchico.edu>
2018-09-17 16:20:10 -0700

    luckynumber complete

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Sam Loose
 #include <iostream>
 
 using namespace std;
@@ -10,15 +10,19 @@ int main()
   cout<<"Guess a number between 1-10: ";
   cin>>guess;
 
-
-  //when they guess correctly (7), display:
+  if(guess==7)
+  {
   cout<<"YES! Lucky number 7!\n";
+  }
 
-  //when they guess just off by one (either 6 or 8), display:
+  else if(guess==8 || guess==6)
+  {        
   cout<<"Oh, so close!\n";
-
-  //when they guess any other number, display:
+  }
+  
+  else
+  {
   cout<<"Waaay off, sorry.\n";
-
+  }
   return 0;
 }
